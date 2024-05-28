a71131353bf95e6f6f3161b1830554658f5c4edd
aditya-prakash-singh <126975871+aditya-prakash-singh@users.noreply.github.com>
2024-05-28 15:02:34 +0530

    therapy over text

---
+++ userlogincheck.php
@@ -0,0 +1,9 @@
+<!-- 
+    
+Yaha Login Check lagana hai.
+
+1. Password galt pe popup/error -> same page(chatlog.html) open rahega.
+2. No user exist pe new tuple add -> chatwithus.html page open hoga.
+3. Password correct + user exist pe purana data show  -> chatwithus.html page open hoga, purane data ke saath.
+
+-->
